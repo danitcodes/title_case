f871f093e12af9c921e1f55eea742b78d304eae6
Danielle Thompson <danithompson74@gmail.com>
2021-06-07 21:16:59 -0700

    Create 1st test & purposely fail

--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -2,5 +2,7 @@ require('rspec')
 require('title_case')
 
 describe('#title_case') do
-  #specs go here
+  it("capitalizes the first letter of a word") do
+    expect(title_case("beowulf")).to(eq("Beowulf"))
+  end
 end
