1a28297feaf4097c3c2d422ac375b138e90ba6d4
Danielle Thompson <danithompson74@gmail.com>
2021-06-07 22:16:26 -0700

    Practice debugging w/ binding.pry

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,7 +1,16 @@
-def title_case(title)
-  split_sentence = title.split
-  split_sentence.each do |word|
-    word.capitalize!()
+# def title_case(title)
+#   split_sentence = title.split
+#   split_sentence.each do |word|
+#     word.capitalize!()
+#   end
+#   split_sentence.join(" ")
+# end
+
+def title_case(sentence)
+  split_sentence = sentence.split()
+binding.pry
+  split_sentence.each() do |word|
+    word.capitalize()
   end
   split_sentence.join(" ")
 end
--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -1,11 +1,25 @@
+# require('rspec')
+# require('title_case')
+
+# describe('#title_case') do
+#   it("capitalizes the first letter of a word") do
+#     expect(title_case("beowulf")).to(eq("Beowulf"))
+#   end
+#   it("capitalizes the first letter of all words in a multiple word title") do
+#     expect(title_case("the color purple")).to(eq("The Color Purple"))
+#   end
+# end
+
 require('rspec')
+require('pry')
 require('title_case')
 
-describe('#title_case') do
+describe('title_case') do
   it("capitalizes the first letter of a word") do
-    expect(title_case("beowulf")).to(eq("Beowulf"))
+    expect(title_case("beowulf")).to eq(Beowulf)
   end
+
   it("capitalizes the first letter of all words in a multiple word title") do
-    expect(title_case("the color purple")).to(eq("The Color Purple"))
+    expect(("the color purple").title_case).to eq("The Color Purple")
   end
 end
