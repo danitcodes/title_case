58d4e02884c5c4ada189594c79dce35f4e020a4f
Danielle Thompson <danithompson74@gmail.com>
2021-06-07 22:06:46 -0700

    Purposely fail 2nd leap yr test

--- spec/leap_year_spec.rb
+++ spec/leap_year_spec.rb
@@ -5,4 +5,8 @@ describe('#leap_year?') do
   it("is false for a year not divisible by 4") do
     expect(leap_year?(1993)).to(eq(false))
   end
+
+  it("is true for most years divisible by 4") do
+    expect(leap_year?(2004)).to(eq(true))
+  end
 end
