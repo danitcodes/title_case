a60ae2eb967c3b352fd6a04f8048ff0624bbc946
Danielle Thompson <danithompson74@gmail.com>
2021-06-07 22:09:23 -0700

    Refactor leap_year method

--- lib/leap_year.rb
+++ lib/leap_year.rb
@@ -1,8 +1,4 @@
 def leap_year?(year)
-  if year.%(4).eql?(0)
-    #modulo method which gives remainder of dividing the # by 4
-    true
-  else
-    false
-  end
+  year.%(4).eql?(0)
+  #modulo method which gives remainder of dividing the # by 4
 end
